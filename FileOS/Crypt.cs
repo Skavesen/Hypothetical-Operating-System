d0adc6597ae154f0c8eebe9edccbd386089847c1
Skavesen <92335820+Skavesen@users.noreply.github.com>
2021-10-11 23:14:04 +0300

    Add files via upload

---
+++ FileOS/Crypt.cs
@@ -0,0 +1,58 @@
+﻿
+using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace FileOS.Functions
+{
+    static class Heshing
+    {
+        public static char[] Hash(string input, int lenght)
+        {
+            using (SHA256Managed sha256 = new SHA256Managed())
+            {
+                var HASH = sha256.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var SB = new StringBuilder(HASH.Length * 2);
+
+                foreach (byte B in HASH)
+                {
+                    SB.Append(B.ToString("X2"));
+                }
+                return SB.ToString().Substring(0, lenght).ToCharArray();
+            }
+        }
+    }
+    public static class Converts
+    {
+        public static byte[] GetBytes<T>(T str)
+        {
+            int Size_struct = Marshal.SizeOf(typeof(T));
+            byte[] arr = new byte[Size_struct];
+
+            IntPtr ptr = Marshal.AllocHGlobal(Size_struct);
+            Marshal.StructureToPtr(str, ptr, true);
+            Marshal.Copy(ptr, arr, 0, Size_struct);
+            Marshal.FreeHGlobal(ptr);
+            return arr;
+        }
+        public static void GetStrucFromBytes<T>(byte[] arr, out T t)
+        {
+            int Size_struct = Marshal.SizeOf(typeof(T));
+            IntPtr ptr = Marshal.AllocHGlobal(Size_struct);
+
+            Marshal.Copy(arr, 0, ptr, Size_struct);
+
+            t = (T)Marshal.PtrToStructure(ptr, typeof(T));
+            Marshal.FreeHGlobal(ptr);
+        }
+    }
+    enum StatusCodeFS
+    {
+        SetNextBlock,
+        EndFile,
+        SectorEmpty
+    }
+}
---
+++ FileOS/Functions/Control.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileOS.Functions
+{
+    class Control
+    {
+        public static string[] AL = { "---", "--x", "-w-", "-wx", "r--", "r-x", "rw-", "rwx" };
+        public static string ConvertAccessLevel(string Input)
+        {
+            string level = "";
+            for (int i = 0; i < Input.Length; i++)
+            {
+                for (int j = 0; j < AL.Length; j++)
+                {
+                    if (Convert.ToInt32(Convert.ToString(Input[i])) == j)
+                        level += AL[j];
+                }
+            }
+            return level;
+        }
+    }
+}
